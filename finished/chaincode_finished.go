8e9bdbcda4ae52560f851ba2cd3b720f21ae311d
margacanuto <marga.canuto@gmail.com>
2016-12-14 11:50:59 -0500

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -69,9 +69,9 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
-		var msg = "Volto logo!"		
-		return []byte(msg), nil 		
-		//return t.read(stub, args)
+		//var msg = "Volto logo!"		
+		//return []byte(msg), nil 		
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
